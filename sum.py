feb34cf9eced6f173132fd1f127c6cbc3fa8986e
KamilS25 <skr2577@yandex.ru>
2026-09-11 19:17:13 +0300

    Add new variable for sum.py

--- sum.py
+++ sum.py
@@ -1,6 +1,7 @@
 a = int(input("Введите первое число: "))
 b = int(input("Введите второе число: "))
+c = int(input("Введите третье число: "))
 
-result = a + b
+result = a + b + c
 
 print("Сумма чисел:", result)
